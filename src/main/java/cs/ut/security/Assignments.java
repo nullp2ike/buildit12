10fd719d7613bcb982208186a1cd367d7ef58970
nullp2ike <risko.ruus@gmail.com>
2013-11-25 22:48:06 +0200

    Create views and controllers for security

---
+++ src/main/java/cs/ut/security/Assignments.java
@@ -0,0 +1,17 @@
+package cs.ut.security;
+import javax.persistence.ManyToOne;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Assignments {
+	
+	@ManyToOne
+	private Users userBuildit;
+	@ManyToOne
+	private Authorities authority;
+}
